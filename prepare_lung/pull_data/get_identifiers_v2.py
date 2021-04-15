08dde86913e44e2e209f6af1506800e875fd7aa2
Pengyu <pyuan2@central.uh.edu>
2021-04-15 09:48:00 -0500

    [Update] Generate identifiers file for Incidental_Lung_Mamta dataset.

--- prepare_lung/pull_data/get_identifiers_v2.py
+++ prepare_lung/pull_data/get_identifiers_v2.py
@@ -9,27 +9,45 @@ def main(args):
     MRNs = biopsy_sheet["MRN"]
     unique_MRNs = MRNs.apply(lambda x: '{0:0>9}'.format(x)).unique()
 
-    chest_CT_sheet["MRN"] = chest_CT_sheet["MRN"].apply(lambda x: '{0:0>9}'.format(x))
-    chest_CT_with_biopsy = chest_CT_sheet[chest_CT_sheet['MRN'].isin(unique_MRNs)]
+    labeled_sheet = infos[3]
+    biopsy_result = labeled_sheet["Results (M-Malignant or B-Benign)"]
+    existId = (biopsy_result == "M") | (biopsy_result == "B")
+    labeled_sheet = labeled_sheet[existId]
+    MRNs = labeled_sheet["MRN"].apply(lambda x: '{0:0>9}'.format(x))
+    scan_date = labeled_sheet["Biopsy Date"]
+    scan_date = scan_date.apply(lambda x: x.strftime("%Y%m%d"))
+    biopsy_result = labeled_sheet["Results (M-Malignant or B-Benign)"]
 
-    scan_date = chest_CT_with_biopsy["Procedure Begin Date"]
-    search_Start = scan_date - pd.DateOffset(days=1)
-    search_End = scan_date + pd.DateOffset(days=1)
-    search_Start = search_Start.apply(lambda x: x.strftime("%Y%m%d"))
-    search_End = search_End.apply(lambda x: x.strftime("%Y%m%d"))
-
-    # surgery = infos["Date Of Surgery {1340}"]
-    # searchStart = surgery - pd.DateOffset(months=args.range)
-    # surgery = surgery.apply(lambda x: x.strftime("%Y%m%d"))
-    # searchStart = searchStart.apply(lambda x: x.strftime("%Y%m%d"))
-    pid = chest_CT_with_biopsy["MRN"]
-    # pid = pid.apply(lambda x: '{0:0>9}'.format(x))
-    identifiers = pd.concat([pid, search_Start, search_End], axis=1)
+    identifiers = pd.concat([MRNs, scan_date, biopsy_result], axis=1)
     folder = os.path.dirname(args.infos)
-    csvFile = os.path.join(folder, "identifiers_v2.csv")
+    csvFile = os.path.join(folder, "identifiers_mamta_20210414.csv")
     # csvFile = os.path.join("./", "identifiers.csv")
-    identifiers.to_csv(csvFile, index=False, header=False)
+    identifiers.to_csv(csvFile, index=False, header=["MRN", "date", "Category of Disease"])
     print("shape of saved dataframe: ", identifiers.shape)
+
+
+
+    # chest_CT_sheet["MRN"] = chest_CT_sheet["MRN"].apply(lambda x: '{0:0>9}'.format(x))
+    # chest_CT_with_biopsy = chest_CT_sheet[chest_CT_sheet['MRN'].isin(unique_MRNs)]
+    #
+    # scan_date = chest_CT_with_biopsy["Procedure Begin Date"]
+    # search_Start = scan_date - pd.DateOffset(days=1)
+    # search_End = scan_date + pd.DateOffset(days=1)
+    # search_Start = search_Start.apply(lambda x: x.strftime("%Y%m%d"))
+    # search_End = search_End.apply(lambda x: x.strftime("%Y%m%d"))
+    #
+    # # surgery = infos["Date Of Surgery {1340}"]
+    # # searchStart = surgery - pd.DateOffset(months=args.range)
+    # # surgery = surgery.apply(lambda x: x.strftime("%Y%m%d"))
+    # # searchStart = searchStart.apply(lambda x: x.strftime("%Y%m%d"))
+    # pid = chest_CT_with_biopsy["MRN"]
+    # # pid = pid.apply(lambda x: '{0:0>9}'.format(x))
+    # identifiers = pd.concat([pid, search_Start, search_End], axis=1)
+    # folder = os.path.dirname(args.infos)
+    # csvFile = os.path.join(folder, "identifiers_v2_20210414.csv")
+    # # csvFile = os.path.join("./", "identifiers.csv")
+    # identifiers.to_csv(csvFile, index=False, header=False)
+    # print("shape of saved dataframe: ", identifiers.shape)
 
 
 if __name__ == '__main__':
@@ -38,6 +56,6 @@ if __name__ == '__main__':
     # parser.add_argument("--range", type=int, help="range of scan searching before surgery (unit: month)",
     #                     required=False, default=3)
     parser.add_argument("--infos", type=str, help="all patient infos file",
-                        required=False, default="/home/cougarnet.uh.edu/pyuan2/Projects/Incidental_Lung/data/Copy of SLN Data 11.05.2020.CFE.xlsx")
+                        required=False, default="/home/cougarnet.uh.edu/pyuan2/Projects/Incidental_Lung/data/Copy of Copy of SLN Data 11.05.2020.CFE.v1.xlsx")
     arguments = parser.parse_args()
     main(arguments)
